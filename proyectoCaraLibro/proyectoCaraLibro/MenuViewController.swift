976f6cc416c2238eb541412bf84564ffb7005f53
Rodrigo Prieto <rodrigoprietoguevara@gmail.com>
2022-07-06 02:56:43 -0400

    Firestore

--- proyectoCaraLibro/proyectoCaraLibro/MenuViewController.swift
+++ proyectoCaraLibro/proyectoCaraLibro/MenuViewController.swift
@@ -8,25 +8,45 @@
 import UIKit
 import FirebaseAnalytics
 import FirebaseAuth
+import FirebaseFirestore
 
 class MenuViewController: UIViewController{
     
     
     @IBOutlet weak var LogOutButton: UIButton!
     
+    @IBOutlet weak var lastNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     private let email: String
+    lazy var userCollection = Firestore.firestore().collection("users").document(email)
     init(email: String){
         self.email = email
         super.init(nibName: nil, bundle: nil)
     }
+    
     required init?(coder: NSCoder) {
          fatalError("init(coder:) has not been implemented")
     }
     override func viewDidLoad() {
+        
+        
         super.viewDidLoad()
+        
         LogOutButton.layer.cornerRadius = 10
         emailLabel.text = email
+        getUsers()
+        
+    }
+    func getUsers(){
+        userCollection.getDocument{ (document, error) in
+            if let document = document, document.exists {
+                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                print("Document data: \(dataDescription)")
+            } else {
+                print("Document does not exist")
+            }
+        }
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
